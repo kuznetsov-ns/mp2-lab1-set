e5274306a8d78c90d1ac76fc021f67014da0dfc6
Nikita Kuznetsov <nikita0bond@gmail.com>
2019-09-20 10:26:53 +0300

    Add some realization

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -9,49 +9,77 @@
 
 TBitField::TBitField(int len)
 {
-	//realization..
+	if (len <= 0) throw "Length should be higher than 0";
+	else
+	{
+		BitLen = len;
+		MemLen = (BitLen + 31) / 32;
+		pMem = new TELEM[MemLen];
+		for (int i = 0; i < MemLen; i++)
+			pMem[i] = 0;
+	}
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+	BitLen = bf.BitLen;
+	MemLen = bf.MemLen;
+	pMem = new TELEM(MemLen);
+	for (int i = 0; i < MemLen; i++)
+		pMem[i] = bf.pMem[i];
 }
 
 TBitField::~TBitField()
 {
+	BitLen = 0;
+	delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+	return n / 32;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+	return 1 << (n % 32);
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+  return this->BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+	if (n < 0) throw "Bit number should be higher";
+	if (n > BitLen) throw "Bit number should be lower";
+	unsigned int p = n / 32;
+	unsigned int mask = GetMemMask(n);
+	pMem[p] |= mask;
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
+	if (n < 0) throw "Bit number should be higher";
+	if (n > BitLen) throw "Bit number should be lower";
+	unsigned int ret = pMem[GetMemIndex(n)] &= 0;
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return 0;
+	if (n < 0) throw "Bit number should be higher";
+	if (n > BitLen) throw "Bit number should be lower";
+	unsigned int ret = pMem[GetMemIndex(n)] &= GetMemMask(n);
 }
 
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
+	return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
@@ -66,22 +94,27 @@ int TBitField::operator!=(const TBitField &bf) const // сравнение
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
+	return *this;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
+	return *this;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
+	return *this;
 }
 
 // ввод/вывод
 
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
+	return istr;
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+	return ostr;
 }
